bc07152640cec79b37f1d2d5a2258b1e22ce3e1b
Alex Valente <alexmfv1@gmail.com>
2023-01-13 22:03:42 +0000

    Commads template and todo list. SimpleGit as main git framework

--- alexmfv-ai-autogit/extension.js
+++ alexmfv-ai-autogit/extension.js
@@ -1,9 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const git = require('simple-git');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+//Enumerator for commands
+const AGCommands = {
+	Commit: "AutoGit Commit",
+	ModifyCommit: "AutoGit Modify Commit Message",
+	CommitAndPush: "AutoGit Commit and Push"
+};
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -15,15 +23,20 @@ function activate(context) {
 	console.log('Congratulations, your extension "alexmfv-ai-autogit" is now active!');
 
 	var statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-		statusBarItem.text = "AutoGit";
-		statusBarItem.command = "alexmfv-ai-autogit.openOptionsMenu";
-		statusBarItem.show();
+	statusBarItem.text = "AutoGit";
+	statusBarItem.command = "alexmfv-ai-autogit.openOptionsMenu";
+	statusBarItem.show();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('alexmfv-ai-autogit.openOptionsMenu', function () {
-		
+		//Show quick pick menu with options: AutoGit Commit, AutoGit Modify Commit Message, AutoGit Commit and Push
+		vscode.window.showQuickPick([AGCommands.Commit, AGCommands.CommitAndPush, AGCommands.ModifyCommit]).then(selection => {
+			switch (selection) {
+				case AGCommands.Commit: AutoGitCommit(); break;
+				case AGCommands.CommitAndPush: AutoGitCommitAndPush(); break;
+				case AGCommands.ModifyCommit: AutoGitModifyCommitMessage(); break;
+				default: break;
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
@@ -32,7 +45,23 @@ function activate(context) {
 // This method is called when your extension is deactivated
 function deactivate() {}
 
+//Commands Code
+function AutoGitCommit() {
+	vscode.window.showInformationMessage('Pressed AutoGit Commit Button!');
+}
+
+function AutoGitCommitAndPush() {
+	vscode.window.showInformationMessage('Pressed AutoGit Commit and Push Button!');
+}
+
+function AutoGitModifyCommitMessage() {
+	vscode.window.showInformationMessage('Pressed AutoGit Modify Commit Message Button!');
+}
+
 module.exports = {
 	activate,
-	deactivate
+	deactivate,
+	AutoGitCommit,
+	AutoGitCommitAndPush,
+	AutoGitModifyCommitMessage
 }
